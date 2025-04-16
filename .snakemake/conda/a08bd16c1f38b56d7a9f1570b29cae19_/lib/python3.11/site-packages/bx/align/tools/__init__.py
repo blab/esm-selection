c678d278a9d423f86fb9116472abe062a536ed9b
Cave42 <the.carlos.ave@gmail.com>
2025-04-16 13:12:13 -0700

    remove .snakemake

--- .snakemake/conda/a08bd16c1f38b56d7a9f1570b29cae19_/lib/python3.11/site-packages/bx/align/tools/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Various utilities for working with `bx.align.Alignment` objects.
-"""
-
-from .chop import *  # noqa: F40
-from .fuse import *  # noqa: F40
-from .thread import *  # noqa: F40
-from .tile import *  # noqa: F40
